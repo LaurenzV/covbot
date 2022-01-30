db8e48764d4fd82647c0794f883e507fa22bb880
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-01-30 17:05:55 +0100

    Added naive ui to the dependencies

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <n-button strong secondary type="primary">Primary</n-button>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -35,8 +36,12 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import { NButton } from 'naive-ui';
 
 @Options({
+  components: {
+    NButton
+  },
   props: {
     msg: String
   }
