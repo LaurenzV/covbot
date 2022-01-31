31a0fabdfe890f4c797de87b10da1490050a3a1f
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-01-31 10:10:40 +0100

    Implemented a first version of the chat component

--- frontend/src/components/ChatComponent.vue
+++ frontend/src/components/ChatComponent.vue
@@ -18,7 +18,22 @@ export default {
       messages: [
         {id: 1, self: true, message: "Hey! How's it going?"},
         {id: 2, self: false, message: "Eh, I'm hanging in there. How about you?"},
-        {id: 3, self: true, message: "Can't complain either!"}
+        {id: 3, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 4, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 5, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 6, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 7, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 8, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 9, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 10, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 11, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 12, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 13, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 14, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 15, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 16, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 17, self: false, message: "Eh, I'm hanging in there. How about you?"},
+        {id: 18, self: true, message: "Can't complain either!"}
       ],
       value: null
     }
