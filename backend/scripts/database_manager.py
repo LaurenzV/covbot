514943033a6a012a81eee5988ccf8e22a2d8931f
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-02-01 20:14:59 +0100

    Created new classes DatabaseManager and DatasetHandler

--- backend/scripts/database_manager.py
+++ backend/scripts/database_manager.py
@@ -5,17 +5,21 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, DATE, St
 from sqlalchemy.exc import DatabaseError
 import logging
 
+from scripts.dataset_handler import DatasetHandler
+
 
 class DatabaseManager:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.logger.addHandler(logging.StreamHandler(sys.stdout))
         self.host = os.environ.get("COVBOT_DB_HOST")
         self.port = os.environ.get("COVBOT_DB_PORT")
         self.user = os.environ.get("COVBOT_USER")
         self.password = os.environ.get("COVBOT_PASSWORD")
         self.db_server_link = f"mysql://{self.user}:{self.password}@{self.host}:{self.port}/"
         self.db_name = "covbot"
+
+        self.logger.addHandler(logging.StreamHandler(sys.stdout))
+        self.logger.setLevel(logging.INFO)
 
     def create_database(self) -> None:
         self.logger.info("Creating the database for Covbot...")
@@ -32,6 +36,19 @@ class DatabaseManager:
                 return
 
         self._create_tables()
+        self.update_database()
+
+    def update_database(self) -> None:
+        self.logger.info("Updating the data in the database...")
+
+        dataset_handler = DatasetHandler()
+        covid_cases = dataset_handler.load_covid_cases("../data/owid/new_cases.csv")
+
+        engine = create_engine(self.db_server_link + f"{self.db_name}")
+        db_connection = engine.connect()
+
+        cases_dtypes = {"date": DATE, "country": String(256), "cases": BigInteger}
+        covid_cases.to_sql(name="cases", con=db_connection, if_exists="replace", index=False, dtype=cases_dtypes)
 
     def _create_tables(self) -> None:
         metadata = MetaData()
