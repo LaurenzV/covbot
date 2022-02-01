c332228f1f5f933dc46375f807fdb266cfa49294
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-02-01 20:34:35 +0100

    Added new column that contains the normalized country name

--- backend/lib/database/dataset_handler.py
+++ backend/lib/database/dataset_handler.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
+from lib.nlp.nlp_pipeline import NLPPipeline
+
 
 class DatasetHandler:
+    def __init__(self):
+        self.nlp_pipeline = NLPPipeline()
+
     def load_covid_cases(self) -> pd.DataFrame:
         path = "../../data/owid/new_cases.csv"
         data = pd.read_csv(path).set_index("date").fillna(0).stack().reset_index()
         data.columns = ["date", "country", "cases"]
+        data["country_normalized"] = data.apply(lambda row:
+                                                self.nlp_pipeline.normalize_country_name(row["country"]), axis=1)
         return data
 
     def load_vaccinations(self, path: str) -> pd.DataFrame:
@@ -15,4 +22,3 @@ class DatasetHandler:
 if __name__ == '__main__':
     dataset_handler = DatasetHandler()
     new_cases = dataset_handler.load_covid_cases()
-    # print(set(new_cases.iloc[:, 1]))
