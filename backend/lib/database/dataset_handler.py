723f42bf8d02a78a0ea8f7388b162803eb084cd5
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-02-02 12:44:28 +0100

    Filter out some of the regions from cases dataset

--- backend/lib/database/dataset_handler.py
+++ backend/lib/database/dataset_handler.py
@@ -11,8 +11,13 @@ class DatasetHandler:
         path = "../../data/owid/new_cases.csv"
         data = pd.read_csv(path).set_index("date").fillna(0).stack().reset_index()
         data.columns = ["date", "country", "cases"]
+
         data["country_normalized"] = data.apply(lambda row:
                                                 self.nlp_pipeline.normalize_country_name(row["country"]), axis=1)
+
+        exclude_countries = ["upper middle income", "summer olympics 2020", "lower middle income",
+                             "low income", "international", "world"]
+        data = data[~data["country_normalized"].isin(exclude_countries)]
         return data
 
     def load_vaccinations(self, path: str) -> pd.DataFrame:
@@ -22,3 +27,4 @@ class DatasetHandler:
 if __name__ == '__main__':
     dataset_handler = DatasetHandler()
     new_cases = dataset_handler.load_covid_cases()
+    print(set(new_cases["country_normalized"]))
