be4d1883c189b494899c67a43d516a13676898f4
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-02-06 15:15:39 +0100

    Added some country abbreviations to the NLP pipeline

--- backend/lib/nlp/nlp_pipeline.py
+++ backend/lib/nlp/nlp_pipeline.py
@@ -5,5 +5,24 @@ class NLPPipeline:
     def normalize_country_name(self, country_name: str) -> str:
         country_name = country_name.lower()
         country_name = re.sub(r"-|'|\s*\([^)]+\)", "", country_name)
+        country_name = self._map_country_abbreviations(country_name)
 
         return country_name
+
+    def _map_country_abbreviations(self, string: str) -> str:
+        # TODO: Extend this
+        map = {
+            "macedonia": "north macedonia",
+            "hk": "hong kong",
+            "nz": "new zealand",
+            "democratic republic of congo": "congo",
+            "uk": "united kingdom",
+            "na": "north america",
+            "eu": "european union",
+            "uae": "united arab emirates",
+            "bosnia": "bosnia and herzegovina",
+            "salvador": "el salvador",
+            "virgin islands": "british virgin islands"
+        }
+
+        return map[string] if string in map else string
