8762c4b7103c097764ba494e2959118259a16c54
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-02-14 19:51:55 +0100

    Set up test cases for recognizing the topic

---
+++ backend/tests/topic.py
@@ -0,0 +1,17 @@
+import unittest
+import json
+from lib.nlu.topic import TopicRecognizer, Topic
+
+class TestQueryTopics(unittest.TestCase):
+    def setUp(self):
+        self.topic_recognizer = TopicRecognizer()
+        with open("annotated_queries.json") as query_file:
+            self.queries = json.load(query_file)
+
+        print(self.queries)
+
+    def test_topics(self):
+        for query in self.queries:
+            with self.subTest(query=query):
+                predicted_topic = self.topic_recognizer.recognize_topic(query["query"])
+                self.assertEqual(TopicRecognizer.from_string(query['topic']), predicted_topic)
