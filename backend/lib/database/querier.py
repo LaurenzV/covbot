aa0d137e5a23008be9ad0a2833e4f0e2f4c01af3
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-02-15 16:14:58 +0100

    Removed old intent recognizer

--- backend/lib/database/querier.py
+++ backend/lib/database/querier.py
@@ -5,8 +5,6 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from sqlalchemy.sql.selectable import Select
 from lib.database.entities import Case, Vaccination
-from lib.intention.intent_recognizer import IntentRecognizer
-from lib.intention.intent import Intent, Topic, Area, TimeFrame, Datapoint
 
 
 class Querier:
@@ -14,50 +12,7 @@ class Querier:
         self.engine = DatabaseConnection().create_engine("covbot")
         self.session = Session(self.engine, future=True)
 
-    def get_results(self, intent: Intent) -> Any:
-        selected_table = self._get_table(intent)
-        if selected_table is None:
-            return intent, None
-
-        filtered_location = self._filter_location(intent, selected_table)
-        filtered_time = self._filter_time_frame(intent, filtered_location)
-        results = self.session.execute(filtered_time).scalars().all()
-
-        return results
-
-    def _filter_time_frame(self, intent: Intent, selection: Select) -> Select:
-        if intent.time_frame["type"] == TimeFrame.SINGLE_DAY:
-            return selection.filter_by(date=intent.time_frame["date"])
-        else:
-            raise NotImplementedError()
-
-    def _filter_location(self, intent: Intent, selection: Select) -> Select:
-        if intent.area["type"] == Area.ONE_COUNTRY:
-            return selection.filter_by(country_normalized=intent.area["country"])
-        else:
-            raise NotImplementedError()
-
-    def _get_table(self, intent: Intent) -> Union[Select, None]:
-        if intent.topic["type"] == Topic.NOT_SPECIFIED:
-            return None
-        elif intent.topic["type"] == Topic.SINGLE_TOPIC:
-            if intent.topic["topic"] == "cases":
-                return select(Case)
-            else:
-                return select(Vaccination)
-        elif intent.topic["type"] == Topic.MULTIPLE_TOPICS:
-            raise NotImplementedError()
-        else:
-            raise NotImplementedError()
 
 
 if __name__ == '__main__':
-    sent1 = "How many people were infected on November 27th 2021 in Austria?"
-
-    querier = Querier()
-
-    ir = IntentRecognizer()
-    sent1_intention = ir.get_intent(sent1)
-    print(sent1_intention)
-    result = querier.get_results(sent1_intention)
-    print(result)
+    pass
--- backend/lib/nlp/answer_generator.py
+++ backend/lib/nlp/answer_generator.py
@@ -1,32 +1,9 @@
-from typing import Any
-
-from lib.database.querier import Querier
-from lib.intention.intent import Intent
-from lib.intention.intent_recognizer import IntentRecognizer
 
 
 class AnswerGenerator:
     def __init__(self):
         pass
 
-    def generate_answer(self, intent: Intent, result: Any) -> str:
-        result = result[0]
-        answer = "On {date}, **{cases}** new cases have been reported in {country}.".format(
-            date=f"{result.date:%B %d, %Y}",
-            cases=result.cases,
-            country=result.country
-        )
-
-        return answer
-
 
 if __name__ == '__main__':
-    sent = "How many people were infected on November 27th 2021 in Austria?"
-    ir = IntentRecognizer()
-    querier = Querier()
-    answer_generator = AnswerGenerator()
-
-    intent = ir.get_intent(sent)
-    result = querier.get_results(intent)
-    answer = answer_generator.generate_answer(intent, result)
-    print(answer)
+    pass
