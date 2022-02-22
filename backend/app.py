4b0ca8bf1f6e1219991646752973ab9543b334fb
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-02-22 20:58:57 +0100

    Removed code form flask app

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from lib.database.querier import Querier
-from lib.intention.intent_recognizer import IntentRecognizer
-from lib.nlp.answer_generator import AnswerGenerator
-
 app = Flask(__name__)
 cors = CORS(app)
-
-intent_recognizer = IntentRecognizer()
-querier = Querier()
-answer_generator = AnswerGenerator()
 
 
 @app.route('/')
 def get_reply():  # put application's code here
-    message = request.args.get("message", default="")
-    intent = intent_recognizer.get_intent(message)
-    result = querier.get_results(intent)
-    answer = answer_generator.generate_answer(intent, result)
-    return jsonify({"msg": answer})
+    pass
 
 
 if __name__ == '__main__':
