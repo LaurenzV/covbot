0598a3be506aa1666102d244b94672a114a35d4e
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-03-02 22:47:59 +0100

    The queries are now read automatically from a list instead of stdin

--- backend/terminal_app.py
+++ backend/terminal_app.py
@@ -10,14 +10,34 @@ spacy = get_spacy()
 print("Finished initialization!")
 
 if __name__ == '__main__':
-    while True:
-        question = input("> ")
 
-        if question == "quit":
-            break
+    sentences = [
+        #"How many new cases have been reported in Austria yesterday?",
+        #"Tell me the number of COVID cases in Germany on 28.02.2022",
+        #"number of COVID cases in Serbia on 28.02.2022",
+        "What is the peak number of confirmed cases in Germany",
+        "What is the lowest number of confirmed cases in Australia",
+        "What is the highest number of people vaccinated in Australia",
+    ]
 
-        sentence = list(spacy(question).sents)[0]
-        message = message_builder.create_message(sentence)
+    for sentence in sentences:
+        new_sent = list(spacy(sentence).sents)[0]
+        message = message_builder.create_message(new_sent)
         query_result = querier.query_intent(message)
         answer = answer_generator.generate_answer(query_result)
+        print(sentence)
         print(answer)
+        print()
+
+
+    # while True:
+    #     question = input("> ")
+    #
+    #     if question == "quit":
+    #         break
+    #
+    #     sentence = list(spacy(question).sents)[0]
+    #     message = message_builder.create_message(sentence)
+    #     query_result = querier.query_intent(message)
+    #     answer = answer_generator.generate_answer(query_result)
+    #     print(answer)
