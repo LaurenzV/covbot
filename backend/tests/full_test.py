39e8b95924a508739e690a47f2517f2973800027
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-03-02 22:48:16 +0100

    Added test cases for testing the whole pipeline

---
+++ backend/tests/full_test.py
@@ -0,0 +1,25 @@
+import json
+import pathlib
+
+import pytest
+
+from lib.database.querier import Querier
+from lib.nlg.answer_generator import AnswerGenerator
+from lib.nlu.message import MessageBuilder
+from lib.spacy_components.custom_spacy import get_spacy
+
+with open(pathlib.Path(__file__).parent / "annotated_queries.json") as query_file:
+    queries = json.load(query_file)
+
+spacy = get_spacy()
+message_builder = MessageBuilder()
+querier = Querier()
+answer_generator = AnswerGenerator()
+
+
+@pytest.mark.parametrize("query", queries)
+def test_whole_pipeline(query):
+    new_sent = list(spacy(query["query"]).sents)[0]
+    message = message_builder.create_message(new_sent)
+    query_result = querier.query_intent(message)
+    answer_generator.generate_answer(query_result)
