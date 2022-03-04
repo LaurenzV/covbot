f72ffa8ca273ad34017f24f2e96f4ad4c1265061
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-03-04 12:38:52 +0100

    Refactored custom spacy to only be initialized once it's actually used

--- backend/lib/spacy_components/custom_spacy.py
+++ backend/lib/spacy_components/custom_spacy.py
@@ -1,14 +1,26 @@
+from typing import Optional
+
 import spacy
 from nltk import PorterStemmer
 from spacy.lang.en import Language
 
 from spacy.tokens import Token
 
-nlp: Language = spacy.load("en_core_web_lg")
 stemmer: PorterStemmer = PorterStemmer()
 
 Token.set_extension("stem", getter=lambda t: stemmer.stem(t.lemma_))
 
 
+class CustomSpacy:
+    nlp: Optional[Language] = None
+
+    @staticmethod
+    def get_spacy() -> Language:
+        if CustomSpacy.nlp is None:
+            CustomSpacy.nlp = spacy.load("en_core_web_lg")
+
+        return CustomSpacy.nlp
+
+
 def get_spacy() -> Language:
-    return nlp
+    return CustomSpacy.get_spacy()
