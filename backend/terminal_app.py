6850a700265c0c07ab32839ca497e04ab6e62df6
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-03-04 14:57:28 +0100

    Added annotated queries to the terminal app

--- backend/terminal_app.py
+++ backend/terminal_app.py
@@ -1,3 +1,6 @@
+import json
+import pathlib
+
 from lib.database.querier import Querier
 from lib.nlg.answer_generator import AnswerGenerator
 from lib.nlu.message import MessageBuilder
@@ -7,38 +10,21 @@ message_builder = MessageBuilder()
 querier = Querier()
 answer_generator = AnswerGenerator()
 spacy = get_spacy()
-print("Finished initialization!")
 
 if __name__ == '__main__':
 
-    sentences = [
-        #"How many new cases have been reported in Austria yesterday?",
-        #"Tell me the number of COVID cases in Germany on 28.02.2022",
-        #"number of COVID cases in Serbia on 28.02.2022",
-        #"What is the peak number of confirmed cases in Germany",
-        #"What is the lowest number of confirmed cases in Australia",
-        #"What is the highest number of people vaccinated in Australia",
-        "How many new Covid cases are there in the UK today?"
-    ]
+    with open(pathlib.Path.cwd() / "tests" / "annotated_queries.json") as query_file:
+        queries = json.load(query_file)
+
+    sentences = ["Tell me the number of positive cases in denmark"]
 
     for sentence in sentences:
         new_sent = list(spacy(sentence).sents)[0]
         message = message_builder.create_message(new_sent)
+        print("Reached")
         query_result = querier.query_intent(message)
+        print("Passed")
         answer = answer_generator.generate_answer(query_result)
         print(sentence)
         print(answer)
         print()
-
-
-    # while True:
-    #     question = input("> ")
-    #
-    #     if question == "quit":
-    #         break
-    #
-    #     sentence = list(spacy(question).sents)[0]
-    #     message = message_builder.create_message(sentence)
-    #     query_result = querier.query_intent(message)
-    #     answer = answer_generator.generate_answer(query_result)
-    #     print(answer)
