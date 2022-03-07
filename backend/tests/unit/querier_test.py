6b98adc282ea953c006f38e8e75f773c512aba2f
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-03-07 12:58:31 +0100

    Fixed bug in a test case

--- backend/tests/unit/querier_test.py
+++ backend/tests/unit/querier_test.py
@@ -298,7 +298,7 @@ def test_check_when_maximum_new_cases_in_austria(querier, session):
     qr: QueryResult = querier.query_intent(msg)
 
     assert qr.result_code == QueryResultCode.SUCCESS
-    assert qr.result == current_day - timedelta(days=1)
+    assert qr.result.value == current_day - timedelta(days=1)
 
 
 with open(pathlib.Path(__file__).parent.parent / "annotated_queries.json") as query_file:
