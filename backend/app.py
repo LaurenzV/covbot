d2f54d00f900f8e395165a501391673bc73f202a
Laurenz Stampfl <laurenz.vallaris+github@gmail.com>
2022-03-09 22:17:09 +0100

    The DatabaseUpdater is now integrated into the Flask app

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -6,6 +8,8 @@ from lib.nlg.answer_generator import AnswerGenerator
 from lib.nlu.message import MessageBuilder
 from lib.spacy_components.custom_spacy import CustomSpacy
 from lib.util.logger import ServerLogger, MessageLogger
+from lib.database.dataset_updater import DatasetUpdater
+import threading
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -17,6 +21,19 @@ spacy = CustomSpacy.get_spacy()
 server_logger: ServerLogger = ServerLogger(__name__)
 server_logger.info("Successfully started the web server... Starting listening to requests now.")
 message_logger: MessageLogger = MessageLogger(__name__)
+dataset_updater: DatasetUpdater = DatasetUpdater()
+
+
+def start_dataset_updater_loop():
+    while True:
+        dataset_updater.start()
+        server_logger.info("Waiting 2 hours before attempting the next update...")
+        time.sleep(10)
+
+
+t1 = threading.Thread(target=start_dataset_updater_loop)
+t1.start()
+
 
 @app.route('/')
 def get_reply():  # put application's code here
@@ -29,13 +46,14 @@ def get_reply():  # put application's code here
         server_logger.info(f"Successfully queried the message with the result {query_result}.")
         answer = answer_generator.generate_answer(query_result)
         server_logger.info(f"Successfully generated the answer {answer.__repr__()}.")
-        message_logger.info(f"QUERY: {raw_message.__repr__()}; ANSWER: {answer.__repr__()}")
+        message_logger.info(f"QUERY: {raw_message}; ANSWER: {answer}")
         return jsonify({"msg": answer})
     except Exception:
         server_logger.exception(f"Error occurred while processing the message {raw_message.__repr__()}")
-        message_logger.info(f"QUERY: {raw_message.__repr__()}; ANSWER: ERROR")
+        message_logger.info(f"QUERY: {raw_message}; ANSWER: ERROR")
         raise
 
 
 if __name__ == '__main__':
     app.run()
+    print("Reached")
