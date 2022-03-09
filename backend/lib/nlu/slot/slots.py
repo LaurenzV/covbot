2948355f57c155067fea5669cfa8e52094fefee8
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-03-09 13:26:31 +0100

    Added documentation for slots

--- backend/lib/nlu/slot/slots.py
+++ backend/lib/nlu/slot/slots.py
@@ -9,16 +9,22 @@ from lib.nlu.slot.location import LocationRecognizer
 
 @dataclass
 class Slots:
+    """Class representing the slots of a query.
+    date: The date mentioned in the query.
+    location: The location mentioned in the query.
+    """
     date: Optional[Date]
     location: Optional[str]
 
 
 class SlotsFiller:
+    """Class providing helper slots to extract the slots from text."""
     def __init__(self):
         self._date_recognizer = DateRecognizer()
         self._location_recognizer = LocationRecognizer()
 
     def fill_slots(self, span: Span) -> Slots:
+        """Returns the filled slots for a span."""
         date = self._date_recognizer.recognize_date(span)
         location = self._location_recognizer.recognize_location(span)
 
