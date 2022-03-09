ca3de77125f77076c32db6994ffe81c3fa0d76b2
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-03-09 15:28:07 +0100

    Added second logger for logging the messages themselves

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from lib.database.querier import Querier
 from lib.nlg.answer_generator import AnswerGenerator
 from lib.nlu.message import MessageBuilder
 from lib.spacy_components.custom_spacy import CustomSpacy
-from lib.util.logger import ServerLogger
+from lib.util.logger import ServerLogger, MessageLogger
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -14,23 +14,26 @@ message_builder: MessageBuilder = MessageBuilder()
 querier: Querier = Querier()
 answer_generator: AnswerGenerator = AnswerGenerator()
 spacy = CustomSpacy.get_spacy()
-logger: ServerLogger = ServerLogger(__name__)
-logger.info("Successfully started the web server... Starting listening to requests now.")
+server_logger: ServerLogger = ServerLogger(__name__)
+server_logger.info("Successfully started the web server... Starting listening to requests now.")
+message_logger: MessageLogger = MessageLogger(__name__)
 
 @app.route('/')
 def get_reply():  # put application's code here
     raw_message: str = request.args.get("msg", default="")
     try:
-        logger.info(f"Received a new message {raw_message.__repr__()}.")
+        server_logger.info(f"Received a new message {raw_message.__repr__()}.")
         message = message_builder.create_message(spacy(raw_message)[:])
-        logger.info(f"Successfully converted the message to {message}.")
+        server_logger.info(f"Successfully converted the message to {message}.")
         query_result = querier.query_intent(message)
-        logger.info(f"Successfully queried the message with the result {query_result}.")
+        server_logger.info(f"Successfully queried the message with the result {query_result}.")
         answer = answer_generator.generate_answer(query_result)
-        logger.info(f"Successfully generated the answer {answer.__repr__()}.")
+        server_logger.info(f"Successfully generated the answer {answer.__repr__()}.")
+        message_logger.info(f"QUERY: {raw_message.__repr__()}; ANSWER: {answer.__repr__()}")
         return jsonify({"msg": answer})
     except Exception:
-        logger.exception(f"Erorr occurred while processing the message {raw_message.__repr__()}")
+        server_logger.exception(f"Error occurred while processing the message {raw_message.__repr__()}")
+        message_logger.info(f"QUERY: {raw_message.__repr__()}; ANSWER: ERROR")
         raise
 
 
--- backend/lib/util/logger.py
+++ backend/lib/util/logger.py
@@ -1,9 +1,12 @@
 import logging
+import os
+import pathlib
 from logging.handlers import TimedRotatingFileHandler
 import sys
 
 
-log_format: str = "%(asctime)s %(levelname)s - %(module)s: %(message)s"
+server_log_format: str = "%(asctime)s %(levelname)s - %(module)s: %(message)s"
+log_path: pathlib.Path = pathlib.Path(os.environ.get("COVBOT_LOGS"))
 
 
 class ColoredFormatter(logging.Formatter):
@@ -16,11 +19,11 @@ class ColoredFormatter(logging.Formatter):
     reset: str = "\x1b[0m"
 
     FORMATS: dict = {
-        logging.DEBUG: grey + log_format + reset,
-        logging.INFO: grey + log_format + reset,
-        logging.WARNING: yellow + log_format + reset,
-        logging.ERROR: red + log_format + reset,
-        logging.CRITICAL: bold_red + log_format + reset
+        logging.DEBUG: grey + server_log_format + reset,
+        logging.INFO: grey + server_log_format + reset,
+        logging.WARNING: yellow + server_log_format + reset,
+        logging.ERROR: red + server_log_format + reset,
+        logging.CRITICAL: bold_red + server_log_format + reset
     }
 
     def format(self, record: logging.LogRecord) -> str:
@@ -32,7 +35,7 @@ class ColoredFormatter(logging.Formatter):
 class Formatter(logging.Formatter):
     """A formatter that allows printing logs using the custom output format."""
     def format(self, record: logging.LogRecord) -> str:
-        formatter: logging.Formatter = logging.Formatter(log_format)
+        formatter: logging.Formatter = logging.Formatter(server_log_format)
         return formatter.format(record)
 
 
@@ -41,7 +44,7 @@ class ServerLogger(logging.Logger):
     def __init__(self, name: str):
         super().__init__(name)
 
-        rotating_file_handler: TimedRotatingFileHandler = TimedRotatingFileHandler("server_log", "M")
+        rotating_file_handler: TimedRotatingFileHandler = TimedRotatingFileHandler(log_path / "server_log", "D")
         rotating_file_handler.setLevel(logging.INFO)
 
         stream_handler: logging.StreamHandler = logging.StreamHandler(sys.stdout)
@@ -52,3 +55,16 @@ class ServerLogger(logging.Logger):
 
         self.addHandler(stream_handler)
         self.addHandler(rotating_file_handler)
+
+
+class MessageLogger(logging.Logger):
+    """Logger that can be used to log the queries and their response."""
+    def __init__(self, name: str):
+        super().__init__(name)
+
+        file_handler: logging.FileHandler = logging.FileHandler(log_path / "messages_log")
+        file_handler.setLevel(logging.INFO)
+
+        file_handler.setFormatter(Formatter())
+
+        self.addHandler(file_handler)
