6a616b5e07cadd34824c1bd0cdca9ce2d6aa48b6
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-03-09 13:11:20 +0100

    Added documentation

--- backend/lib/nlu/intent/value_type.py
+++ backend/lib/nlu/intent/value_type.py
@@ -5,6 +5,13 @@ from typing import Optional
 
 
 class ValueType(Enum):
+    """Class representing the value type, so what kind of value we are trying to extract.
+    NUMBER: When trying to extract a number, such as the number of positive cases on a day or the highest number
+    of vaccines administered on a certain day.
+    DAY: When we are trying to extract a day - for example "When was the highest number of cases recorded in Austria?".
+    LOCATION: When we are trying to extract a location - for example "Where was the lowest number of cases reported?".
+    UNKNOWN: When the value type is unknown.
+    """
     NUMBER = 1
     DAY = 2
     LOCATION = 3
