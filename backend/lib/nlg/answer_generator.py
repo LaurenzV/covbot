c40530efb23829aa54491b05b5ac3c91ddbde396
Laurenz Stampfl <e11809648@student.tuwien.ac.at>
2022-03-09 11:08:38 +0100

    Added documentation

--- backend/lib/nlg/answer_generator.py
+++ backend/lib/nlg/answer_generator.py
@@ -15,13 +15,29 @@ from lib.nlu.slot.location import Location
 
 
 class AnswerGenerator:
+    """Class that provides methods to generate an answer in natural language.
+
+    The answer patterns used by this class will be read from the answers.yaml file. The patterns im the answers.yaml
+    file must be provided for each possible combination of intent. It is currently not possible to reuse other patterns,
+    so each combination must be added manually.
+
+    Words wrapped in curly brackets, for example "{location}", allow to
+    specify the fields that must be passed on to the answer pattern. It is also possible to define optional fields
+    by wrapping the field including any additional text in <>. For example, let's consider the pattern
+    "There have been {number} cases< in {location}>.". In this case, "number" is a mandatory field, so it must be passed
+    as a parameter. The location paramter is wrapped in <>, meaning that it is optional. Furthermore, the word "in" is
+    also inside the <> brackets. This means that in the event that no location parameter is passed to the answer
+    generator, the "in" will be omitted as well. So two possible answer generated from this pattern might be
+    "There have been 1,000 cases." and "There have been 1,000 cases in Austria.".
+    """
     def __init__(self):
         with open(pathlib.Path(__file__).parent / "answers.yaml") as answers_file:
             self.answers: dict = yaml.safe_load(answers_file)
 
     def generate_answer(self, query_result: QueryResult) -> str:
-
+        """Generates an answer based on a QueryResult object."""
         if query_result.result_code == QueryResultCode.NO_DATA_AVAILABLE_FOR_DATE:
+            # Latest date contains the most recent date for which we have information available.
             return random.choice(self.answers[query_result.result_code.NO_DATA_AVAILABLE_FOR_DATE.name]).format(
                 date=query_result.message.slots.date,
                 latest_date=Date.generate_date_message(query_result.information["latest"], include_preposition=False),
@@ -34,9 +50,11 @@ class AnswerGenerator:
         elif query_result.result_code == QueryResultCode.NOT_EXISTING_LOCATION:
             return random.choice(self.answers[query_result.result_code.name]).format(location=query_result.message.slots.location)
         elif query_result.result_code == QueryResultCode.INVALID_MESSAGE:
+            # Check whether the invalid message is due to a user error or a not considered case server-side.
             if query_result.information["message_validation_code"] in MessageValidationCode.get_user_query_error_codes():
                 return random.choice(self.answers[query_result.result_code.name][query_result.information["message_validation_code"].name])
             else:
+                # An unconsidered case occurred, maybe a special combination of the various fields for the intent.
                 return random.choice(self.answers[query_result.result_code.name]["INTERNAL_ERROR"])
         elif query_result.result_code == QueryResultCode.SUCCESS:
             return self._generate_success_answer(query_result)
@@ -44,6 +62,7 @@ class AnswerGenerator:
             raise NotImplementedError()
 
     def _generate_success_answer(self, query_result: QueryResult) -> str:
+        """Generates an answer based on the assumption that the query was successful."""
         message: Message = query_result.message
         sub_fields: dict = self._get_sub_fields_from_slots_for_success_message(query_result)
 
@@ -51,6 +70,7 @@ class AnswerGenerator:
                                  [message.intent.value_type.name][message.intent.calculation_type.name]
                                  [message.intent.measurement_type.name])
 
+        # Remove opional fields if they are not in the parameters
         if "location" not in sub_fields:
             unformatted_answer = re.sub(r"<[^<]*{location}[^>]*>", "", unformatted_answer)
         if "date" not in sub_fields:
@@ -61,14 +81,17 @@ class AnswerGenerator:
         try:
             return unformatted_answer.format(**sub_fields)
         except KeyError:
+            # We tried to format with a key that doesn't exist in the answer pattern
             raise NotImplementedError()
 
     def _get_sub_fields_from_slots_for_success_message(self, query_result: QueryResult) -> dict:
+        """Extracts the additional information from the query results into a dict."""
         slots: Slots = query_result.message.slots
         result = query_result.result
         value_dict = dict()
 
         if result is not None:
+            # Format integers properly
             if isinstance(result, (int, float, decimal.Decimal)):
                 value_dict["result"] = f"{int(result):,}"
             else:
@@ -83,6 +106,7 @@ class AnswerGenerator:
             value_dict["date"] = Date.generate_date_message(slots.date)
 
         return value_dict
+
 
 if __name__ == '__main__':
     with open(pathlib.Path(__file__).parent / "answers.yaml") as answers_file:
